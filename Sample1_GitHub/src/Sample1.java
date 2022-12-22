662a49846c22d4a52a61922cdb1cd35a19569a09
kiranm889 <kiranm2636@gmail.com>
2022-12-22 15:45:21 +0530

    output function

--- Sample1_GitHub/src/Sample1.java
+++ Sample1_GitHub/src/Sample1.java
@@ -1,7 +1,17 @@
 public class Sample1 {
+	
+	public static boolean output(int a,int b) {
+		if(a<b) {
+			return true;
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Havin some files edited");
+		boolean op=output(10,22);
+		System.out.println(op);
 	}
 
 }
